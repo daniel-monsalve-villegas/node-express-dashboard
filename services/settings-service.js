74eef01930a2fe25b9b68cb97a9cdda0edc1a8be
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-01 18:54:53 -0400

    fixed relative direction

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const settingsFilePath = path.join(__dirname, 'json/settings.json');
+const settingsFilePath = path.join(__dirname, '../json/settings.json');
 
 function getSettings() {
   return {};
