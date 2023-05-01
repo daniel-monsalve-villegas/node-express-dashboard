027d897efa6ff3e6ed9514dcbc31e475605fbfa9
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-01 19:12:55 -0400

    Set the current directory from settings when loading the select file page

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/', (req, res, next) => {
 
 /* GET select file. */
 router.get('/select-file', (req, res, next) => {
+  fileService.setcwd(getDefaultDir());
   res.render('select-file', { title: 'Select Log File' });
 });
 
