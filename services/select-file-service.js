4ca7fded3bf9e671146815ca6b05908f2bc9e05b
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-01 17:42:41 -0400

    Implement the getDirecotryContents() function

--- services/select-file-service.js
+++ services/select-file-service.js
@@ -1,7 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
-function getDirectoryContents(files, currentDir, query) {}
+function getDirectoryContents(files, currentDir, query) {
+  const data = [];
+  files.forEach((file) => {
+    if (isDirectory(currentDir, file)) {
+      data.push({
+        name: file,
+        isDirectory: true,
+        path: path.join(query, file),
+      });
+    } else {
+      data.push({
+        name: file,
+        isDirectory: false,
+        path: path.join(query, file),
+        currentDir,
+      });
+    }
+  });
+  return data;
+}
 
 function isDirectory(currentDir, file) {
   const fileInfo = fs.statSync(path.join(currentDir, file));
