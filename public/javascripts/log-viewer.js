2e3f1a6d1996e08e9056f5c8dfd2b42a1b332705
daniel-monsalve-villegas <danielmonsalve214164@correo.itm.edu.co>
2023-05-01 13:42:30 -0400

    Display the logs in the log window

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -9,5 +9,6 @@ connection.onopen = () => {
 };
 
 connection.onmessage = (event) => {
-  logWindow.innerHTML = event.data;
+  const logs = event.data.split('\n').join('<hr>');
+  logWindow.innerHTML = logs;
 };
